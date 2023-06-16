0484c91e4cef209661ecaf514e1e6eda3719c909
Koray ÇELİK <97339180+kryC1@users.noreply.github.com>
2023-06-16 13:12:53 +0300

    Update urls.py

--- s2i_django/stories2image/urls.py
+++ s2i_django/stories2image/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='s2i-home'),
+    path('about', views.about, name='s2i-about'),
+    path('contact', views.contact, name='s2i-contact')
 ]
